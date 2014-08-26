d510c72faf1fe8511e8be779b3b61ac917a03751
fisherrm <fisherr@uwec.edu>
2014-08-26 13:38:49 -0500

    Parse JavaScript SDK test

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,10 @@
-
+//Parse related keys
+Parse.initialize("kPoCeBgdXvmx1NnjRjubhnDEpQQkQBWAwdDtcDRB", "4b4MdiH84VbGmPZOxz34LDzBBz9ILpm9z0jlnUZl");
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
